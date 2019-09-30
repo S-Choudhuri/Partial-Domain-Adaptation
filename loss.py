f3dafee4931e6fc026e49f8fe3e71a5ae2252b24
S-Choudhuri <schoud13@asu.edu>
2019-09-30 12:35:11 -0700

    modified loss.py

--- loss.py
+++ loss.py
@@ -1,1 +1,20 @@
 import numpy as np
+
+def weight_comp(y):
+	cw = np.array(([0] * y.shape[1]))
+	cn = np.array(([0] * y.shape[1]))
+    yi = y.argmax(1)
+
+    for i in range(ys.shape[0]):
+        cw[yi[i]] = cw[yi[i]] + y[i, yi[i]]
+        cn[yi[i]] = cn[yi[i]] + y[i, yi[i]]
+ 
+    return cw / yi
+
+def ingegrated_loss_weight(ws, wt):
+	return 1 - np.absolute(ws-wt)
+
+def loss_weight(ys, yt):
+	ws = weight_comp(ys)
+	wt = weight_comp(yt)	
+    return integrated_loss_weight(ws, wt)
